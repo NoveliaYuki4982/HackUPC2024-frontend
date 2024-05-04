8a26ec121b6677ab570325b3a4fd35ba064c39da
NoveliaYuki <alguien.diferente09@gmail.com>
2024-05-04 17:56:02 +0200

    feat: gyro

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/gyroscope", function (req, res) {
+    res.sendFile(__dirname + "/gyroscope.html");
+});
+
 app.get("/start", function (req, res) {
     res.set('Content-Type', 'text/html');
     res.send(JSON.stringify("Pepe"));
