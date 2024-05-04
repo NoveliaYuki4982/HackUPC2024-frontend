5cbc527901ec5769130fc3851d4336aefe03512d
NoveliaYuki <alguien.diferente09@gmail.com>
2024-05-04 17:44:43 +0200

    fix: solved bug

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-const express = require("express");
-const app = express();
-
-app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html");
-});
-
-app.listen(8000, function () {
-    console.log("Server is running on localhost:8000");
-});
-
 app.get("/start", function (req, res) {
     res.set('Content-Type', 'text/html');
     res.send(JSON.stringify("Pepe"));
